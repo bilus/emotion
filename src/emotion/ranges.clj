b671f04275c00ffa8475f17018069b23f20577b6
Martin Bilski <martinb@criticue.com>
2014-07-25 01:12:08 +0200

    Better ranges for triangular terms.

--- src/emotion/ranges.clj
+++ src/emotion/ranges.clj
@@ -2,23 +2,23 @@
   (:use emotion.examples))
 
 (defn- make-range
-  [[prev-from prev-to] [width offset]]
+  [[prev-from prev-to] [from-offset to-offset]]
   (let [prev-width (- prev-to prev-from)
-        from (+ prev-from (* offset prev-width))
-        to (+ from width)]
+        from (+ prev-from (* from-offset prev-width))
+        to (+ prev-to to-offset)]
     [from to]))
 
 
 (defn make-ranges
   "Converts a vector of relative triangular terms widths and offsets (a representation facilitating simpler mutation and cross-over) into pairs of ranges for each term's triangle."
   {:test (examples
-          (make-ranges [1.0 0.3 0.7
-                       0.0 0.3 0.5]) => (list [0.0 1.0] [0.3 0.6] [0.44999999999999996 1.15])
-          (make-ranges [1
-                        0]) => (list [0 1]))}
+          (make-ranges [0.0 0.3 0.5
+                        1.0 0.3 0.7]) => (list [0.0 2.0] [0.6 2.3] [1.4499999999999997 3.0])
+          (make-ranges [0
+                        1]) => (list [0 2]))}
   [offset-representation]
-  (let [[widths offsets] (partition (/ (count offset-representation) 2) offset-representation)]
-    (->> (map vector widths offsets)
+  (let [[from-offsets to-offsets] (partition (/ (count offset-representation) 2) offset-representation)]
+    (->> (map vector from-offsets to-offsets)
          (reduce #(conj %1 (make-range (last %1) %2)) [[0 1]])
          (next))))
 
@@ -27,11 +27,10 @@
   "Keeps ranges in [0..1] uhm.. range."
   {:test (examples
           (scale-ranges [0 1 0.5 2]) => [0.0 0.5 0.25 1.0]
-          (scale-ranges [0.5 1.5 1 2.5] => [0 0.5 0.25 1]))}
+          (scale-ranges [0.5 1.5 1 2.5]) ~=> [0 0.5 0.25 1])}
   [ranges]
-  (let [r (partition 2 ranges)
-        min-from (first ranges)
-        max-to (reduce #(max %1 (second %2)) 0 r)
-        scale (/ 1.0 (- max-to min-from))]
-    (map (partial * scale) ranges)))
+  (let [min-from (reduce min ranges)
+        max-to (reduce max ranges)
+        scale #(/ (- % min-from) (- max-to min-from))]
+    (map (comp float scale) ranges)))
 
