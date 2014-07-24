f1e1206e3be9e0f3ee5c8b2b4b63896dedd0ee5b
Martin Bilski <martinb@criticue.com>
2014-07-25 01:12:20 +0200

    ...

--- src/emotion/rand.clj
+++ src/emotion/rand.clj
@@ -1,25 +1,25 @@
-    (ns emotion.rand
-      (:use emotion.examples))
+(ns emotion.rand
+  (:use emotion.examples))
 
-    (def ^:dynamic *rand* clojure.core/rand)
+(def ^:dynamic *rand* clojure.core/rand)
 
-    (defn rand-1
-     ([]
-       (*rand* 1))
-     ([n]
-       (*rand* n)))
+(defn rand-1
+ ([]
+   (*rand* 1))
+ ([n]
+   (*rand* n)))
 
-    (defmacro with-rand-seed
-     "Sets seed for calls to random in body. Beware of lazy seqs!"
+(defmacro with-rand-seed
+ "Sets seed for calls to random in body. Beware of lazy seqs!"
 
-     [seed & body]
-      `(let [g# (java.util.Random. ~seed)]
-         (binding [*rand* #(* % (.nextFloat g#))]
-          (with-redefs [rand rand-1]
-            ~@body))))
+ [seed & body]
+  `(let [g# (java.util.Random. ~seed)]
+     (binding [*rand* #(* % (.nextFloat g#))]
+      (with-redefs [rand rand-1]
+        ~@body))))
 
 
-    ;; TODO: Move to a separate file or make work inline.
-    (clojure.test/deftest test-with-rand-seed ((examples
-           (with-rand-seed 4
-             (rand)) ~=> 0.7306094)))
+;; TODO: Move to a separate file or make work inline.
+(clojure.test/deftest test-with-rand-seed ((examples
+       (with-rand-seed 4
+         (rand)) ~=> 0.7306094)))
