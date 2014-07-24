337f03f5cf3c4ea803874309e304b991d4a0f17c
Martin Bilski <martinb@criticue.com>
2014-07-24 12:16:59 +0200

    Rewrite with-rand-seed so it doesn't conflict with built-in rand function but rather uses with-redefs to scope the redefinition.

--- src/emotion/rand.clj
+++ src/emotion/rand.clj
@@ -1,26 +1,21 @@
-(ns emotion.rand
-  (:use emotion.examples)
-  (:refer-clojure :exclude [rand]))
+    (ns emotion.rand
+      (:use emotion.examples)
+      (:refer-clojure :exclude [rand]))
 
-(def ^:dynamic *rand* clojure.core/rand)
+    (defmacro with-rand-seed
+     "Sets seed for calls to random in body. Beware of lazy seqs!"
 
-(defn rand
- []
- (*rand*))
+     [seed & body]
+      `(let [g# (java.util.Random. ~seed)
+            new-rand# #(.nextFloat g#)]
+         (with-redefs [rand new-rand#]
+            ~@body)))
 
-(defmacro with-rand-seed
- "Sets seed for calls to rand in body. Beware of lazy seqs!"
+    ;; TODO: Move to a separate file or make work inline.
+    (clojure.test/deftest test-with-rand-seed ((examples
+           (with-rand-seed 4
+             (rand)) ~=> 0.7306094)))
 
- [seed & body]
-  `(let [g# (java.util.Random. ~seed)
-        new-rand# #(.nextFloat g#)]
-   (binding [*rand* new-rand#]
-      ~@body)))
+    (with-rand-seed 8
+             (rand))
 
-;; TODO: Move to a separate file or make work inline.
-(clojure.test/deftest test-with-rand-seed ((examples
-       (with-rand-seed 4
-         (rand)) ~=> 0.7306094)))
-
-(with-rand-seed 4
-         (rand))
