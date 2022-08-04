27af91fa19c69f14167a438078f3ed4240047358
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-04 15:51:54 +0800

    Delete test/emotion directory

--- test/emotion/core_test.clj
+++
@@ -1,8 +0,0 @@
-(ns emotion.core-test
-  (:require [clojure.test :refer :all]
-            [emotion.core :refer :all]))
-
-;; (deftest a-test
-;;   (testing "FIXME, I fail."
-;;     (is (not (nil? nil)))))
-
